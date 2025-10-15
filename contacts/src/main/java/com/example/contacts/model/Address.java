4653476c256359dc705ca985f37ae2899159c158
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-10-15 23:40:58 +0200

    Update Address entity class to include column annotations

--- contacts/src/main/java/com/example/contacts/model/Address.java
+++ contacts/src/main/java/com/example/contacts/model/Address.java
@@ -15,8 +15,14 @@ public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Column(name = "street")
     private String street;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "province")
     private String province;
 
 }
