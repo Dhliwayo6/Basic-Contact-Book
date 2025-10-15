6417023918db648c654b5d900eb45c05f8743d69
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-10-15 23:39:18 +0200

    Update Contact entity class to include column and contraint annotations

--- contacts/src/main/java/com/example/contacts/model/Contact.java
+++ contacts/src/main/java/com/example/contacts/model/Contact.java
@@ -1,6 +1,7 @@
 package com.example.contacts.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +16,25 @@ public class Contact {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @NotNull(message = "Name cannot be empty")
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "surname")
     private String surname;
+
+    @Column(name = "email")
     private String email;
+
+    @NotNull(message = "Phone number cannot be null")
+    @Column(name = "number")
     private String number;
+
+    @Column(name = "nickname")
     private String nickname;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
 }
