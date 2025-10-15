d29602679fbb3580fe0c7848615928998f953486
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-10-16 00:33:28 +0200

    Add service class to create contact

---
+++ contacts/src/main/java/com/example/contacts/services/contact/CreateContactService.java
@@ -0,0 +1,27 @@
+package com.example.contacts.services.contact;
+
+import com.example.contacts.Command;
+import com.example.contacts.dtos.ContactDTO;
+import com.example.contacts.model.Contact;
+import com.example.contacts.repositories.ContactRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateContactService implements Command<Contact, ContactDTO> {
+
+    private ContactRepository contactRepository;
+
+    public CreateContactService(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    @Override
+    public ResponseEntity<ContactDTO> execute(Contact contact) {
+        Contact contactSaved = contactRepository.save(contact);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ContactDTO(contactSaved));
+
+    }
+
+}
