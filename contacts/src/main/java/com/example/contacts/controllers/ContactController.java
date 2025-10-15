41232d35bf6d6488e4c60373bcb1928358e56ca3
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-10-16 01:21:40 +0200

    Add controller, add post mapping method

---
+++ contacts/src/main/java/com/example/contacts/controllers/ContactController.java
@@ -0,0 +1,38 @@
+package com.example.contacts.controllers;
+
+import com.example.contacts.dtos.ContactDTO;
+import com.example.contacts.model.Contact;
+import com.example.contacts.services.contact.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+    private CreateContactService createContactService;
+    private GetContactsService getContactsService;
+    private GetContactService getContactService;
+    private UpdateContactService updateContactService;
+    private DeleteContactService deleteContactService;
+
+    public ContactController(CreateContactService createContactService,
+                             GetContactsService getContactsService,
+                             GetContactService getContactService,
+                             UpdateContactService updateContactService,
+                             DeleteContactService deleteContactService) {
+        this.createContactService = createContactService;
+        this.getContactsService = getContactsService;
+        this.getContactService = getContactService;
+        this.updateContactService = updateContactService;
+        this.deleteContactService = deleteContactService;
+    }
+
+    @PostMapping("/contact")
+    public ResponseEntity<ContactDTO> createContact(@RequestBody Contact contact) {
+        return createContactService.execute(contact);
+    }
+
+
+}
