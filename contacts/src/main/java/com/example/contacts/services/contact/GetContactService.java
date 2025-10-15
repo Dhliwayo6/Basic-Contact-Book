a57f846834effa74c8d2b171d288b030c62d1f61
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-10-16 00:55:41 +0200

    Add service class to get contact by id

---
+++ contacts/src/main/java/com/example/contacts/services/contact/GetContactService.java
@@ -0,0 +1,29 @@
+package com.example.contacts.services.contact;
+
+import com.example.contacts.Query;
+import com.example.contacts.dtos.ContactDTO;
+import com.example.contacts.model.Contact;
+import com.example.contacts.repositories.ContactRepository;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+public class GetContactService implements Query<Integer, ContactDTO> {
+
+    private ContactRepository contactRepository;
+
+    public GetContactService(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    @Override
+    public ResponseEntity<ContactDTO> execute(Integer id) {
+        Optional<Contact> contactOptional = contactRepository.findById(id);
+
+        if (contactOptional.isPresent()) {
+            return ResponseEntity.ok(new ContactDTO(contactOptional.get()));
+        }
+
+        throw new IllegalArgumentException("Contact not found!");
+    }
+}
