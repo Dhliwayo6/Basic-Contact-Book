5c868baa579b39f6160d7c726e54020f3aef1802
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-10-16 23:10:52 +0200

    Include @ResponseBody annotation in GlobalExceptionHandler

--- contacts/src/main/java/com/example/contacts/exceptions/GlobalExceptionHandler.java
+++ contacts/src/main/java/com/example/contacts/exceptions/GlobalExceptionHandler.java
@@ -2,20 +2,20 @@ package com.example.contacts.exceptions;
 
 import com.example.contacts.dtos.ErrorResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ContactNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
     public ErrorResponse handleContactNotFoundException(ContactNotFoundException e) {
         return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(NoResultsException.class)
     @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
     public ErrorResponse handleNoResultsFoundException(NoResultsException e) {
         return new ErrorResponse(e.getMessage());
     }
