c6ac83056b11c58a3e01b5e1102c0b029614c84b
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-10-16 22:42:55 +0200

    Update ContatctNotFoundException

--- contacts/src/main/java/com/example/contacts/exceptions/ContactNotFoundException.java
+++ contacts/src/main/java/com/example/contacts/exceptions/ContactNotFoundException.java
@@ -1,4 +1,12 @@
 package com.example.contacts.exceptions;
 
-public class ContactNotFoundException {
+import com.example.contacts.enums.ErrorMessages;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ContactNotFoundException extends RuntimeException{
+    public ContactNotFoundException() {
+        super(ErrorMessages.CONTACT_NOT_FOUND.getMessage());
+    }
 }
