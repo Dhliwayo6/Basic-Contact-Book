fedb0c46db3b66674f4fb056a4e4361a9850241f
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-10-16 22:43:59 +0200

    Update GlobalExceptionHandler to incorporate ContactNotFoundException

--- contacts/src/main/java/com/example/contacts/exceptions/GlobalExceptionHandler.java
+++ contacts/src/main/java/com/example/contacts/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,16 @@
 package com.example.contacts.exceptions;
 
+import com.example.contacts.dtos.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ContactNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleContactNotFoundException(ContactNotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
 }
